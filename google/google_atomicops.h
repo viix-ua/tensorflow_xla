f75885471f47a121e5103cd1525e7c4c68ffce44
diixo <anatolii.podoba@gmail.com>
2020-08-25 00:04:41 +0300

    add atomic-ops implementation for Linux-build

--- google/google_atomicops.h
+++ google/google_atomicops.h
@@ -197,7 +197,7 @@ Atomic64 Release_Load(volatile const Atomic64* ptr);
 // GCC.
 #elif defined(__GNUC__)
 #if defined(GOOGLE_PROTOBUF_ARCH_IA32) || defined(GOOGLE_PROTOBUF_ARCH_X64)
-#include <google/protobuf/stubs/atomicops_internals_x86_gcc.h>
+#include "google_atomicops_internals_x86_gcc.h"
 #elif defined(GOOGLE_PROTOBUF_ARCH_ARM) && defined(__linux__)
 #if (((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)) || (__GNUC__ > 4))
 #include <google/protobuf/stubs/atomicops_internals_generic_gcc.h>
