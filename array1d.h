90fb95679d7918234ce9a4f2c7e0504d91afec8f
diixo <anatolii.podoba@gmail.com>
2020-09-05 17:10:57 +0300

    activation fn-s added

--- array1d.h
+++ array1d.h
@@ -9,26 +9,8 @@
 #include "types.h"
 
 
-namespace xla 
+namespace xla
 {
-   template <typename TType>
-   static void SoftPlus(std::vector<TType>& flatten)
-   {
-      for (size_t i = 0; i < flatten.size(); i++)
-      {
-         flatten[i] = std::log(TType(1.0f) + std::exp(flatten[i]));
-      }
-   }
-
-   template <typename TType>
-   static void SoftSign(std::vector<TType>& flatten)
-   {
-      for (size_t i = 0; i < flatten.size(); i++)
-      {
-         flatten[i] = flatten[i] / (fabs(flatten[i]) + 1.f);
-      }
-   }
-
    template <typename TType>
    static void Log(std::vector<TType>& flatten)
    {
@@ -39,33 +21,11 @@ namespace xla
    }
 
    template <typename TType>
-   static void Sigmoid(std::vector<TType>& flatten)
-   {
-      for (size_t i = 0; i < flatten.size(); i++)
-      {
-         flatten[i] = TType(1.f) / (TType(1.f) + std::exp(-flatten[i]));
-      }
-   }
-
-   template <typename TType>
    static void Square(std::vector<TType>& flatten)
    {
       for (size_t i = 0; i < flatten.size(); i++)
       {
          flatten[i] = flatten[i] * flatten[i];
-      }
-   }
-
-   // Exponential linear unit a(exp(features) - 1) if < 0, else feature
-   template <typename TType>
-   static void Elu(std::vector<TType>& flatten)
-   {
-      for (size_t i = 0; i < flatten.size(); i++)
-      {
-         if (flatten[i] <= 0.f)
-         {
-            flatten[i] = std::exp(flatten[i]) - TType(1.f);  // a(exp(x) - 1)
-         }
       }
    }
 
